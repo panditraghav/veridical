f5ce970a63d4572d3186f9c69b2b144833c346a3
Raghav Pandit <panditraghav18@gmail.com>
2023-02-04 16:27:57 +0530

    Fixed problem of AddLinkDialog showing on Converting from markdown

--- packages/plugins/src/AddLinkDialogPlugin/index.tsx
+++ packages/plugins/src/AddLinkDialogPlugin/index.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { LinkNode, $isLinkNode } from "@lexical/link";
-import { $getNodeByKey } from "lexical";
-import { useVeridicalTheme } from "@veridical/utils";
+import React, { useEffect, useRef, useState, useCallback } from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { LinkNode, $isLinkNode } from '@lexical/link';
+import { $getNodeByKey, $getSelection } from 'lexical';
+import { useVeridicalTheme } from '@veridical/utils';
 
 const Y_OFFSET = -35;
 
 type Position = {
     left?: number;
     top?: number;
-    display?: "block" | "none"
+    display?: 'block' | 'none';
 };
 
 function usePosition(linkDomNode: HTMLElement | null): Position {
-    const [position, setPosition] = useState<Position>({ display: "none" })
+    const [position, setPosition] = useState<Position>({ display: 'none' });
     useEffect(() => {
         if (linkDomNode) {
-            const {
-                left,
-                top
-            } = linkDomNode.getBoundingClientRect();
+            const { left, top } = linkDomNode.getBoundingClientRect();
             setPosition({
                 left,
-                top: top + window.scrollY + Y_OFFSET, display: "block"
+                top: top + window.scrollY + Y_OFFSET,
+                display: 'block',
             });
         }
-    }, [linkDomNode])
+    }, [linkDomNode]);
     return position;
 }
 
@@ -33,49 +31,47 @@ export default function AddLinkDialogPlugin() {
     const [editor] = useLexicalComposerContext();
     const [linkNode, setLinkNode] = useState<LinkNode | null>(null);
     const [linkDomNode, setLinkDomNode] = useState<HTMLElement | null>(null);
-    const [link, setLink] = useState<string>("");
-    const inputRef = useRef<HTMLInputElement | null>(null)
+    const [link, setLink] = useState<string>('');
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const position = usePosition(linkDomNode);
     const theme = useVeridicalTheme()?.addLinkDialog;
 
     const saveLink = useCallback(() => {
-        if (!linkNode) return;;
+        if (!linkNode) return;
         editor.update(() => {
             linkNode.setURL(link);
-        })
-    }, [linkNode, link, editor])
+        });
+    }, [linkNode, link, editor]);
 
     useEffect(() => {
         if (linkNode && inputRef.current) {
             inputRef.current.focus();
         }
-    }, [linkNode, inputRef.current])
+    }, [linkNode, inputRef.current]);
 
     useEffect(() => {
-        return editor.registerMutationListener(
-            LinkNode,
-            (nodes, _) => {
-                nodes.forEach((value, key) => {
-                    if (value == "created") {
-                        editor.getEditorState().read(() => {
-                            const linkNode = $getNodeByKey(key);
-                            if ($isLinkNode(linkNode)) {
-                                setLinkNode(linkNode);
-                                setLink(linkNode.getURL())
-                                setLinkDomNode(
-                                    editor.getElementByKey(key)
-                                );
-                            }
-                        });
-                    }
-                });
+        return editor.registerMutationListener(LinkNode, (nodes, payload) => {
+            nodes.forEach((value, key) => {
+                if (value == 'created') {
+                    // return when LinkNode is created using editor state (Markdown)
+                    if (payload.updateTags.has('history-merge')) return;
+                    editor.getEditorState().read(() => {
+                        const linkNode = $getNodeByKey(key);
+                        if ($isLinkNode(linkNode)) {
+                            setLinkNode(linkNode);
+                            setLink(linkNode.getURL());
+                            setLinkDomNode(editor.getElementByKey(key));
+                        }
+                    });
+                }
             });
+        });
     }, [editor]);
 
     if (!linkNode) return null;
     return (
         <input
-            style={{ ...position, position: "absolute" }}
+            style={{ ...position, position: 'absolute' }}
             ref={inputRef}
             type="url"
             className={theme?.input}
@@ -83,13 +79,13 @@ export default function AddLinkDialogPlugin() {
             value={link}
             onChange={(e) => setLink(e.target.value)}
             onBlur={() => {
-                saveLink()
-                setLinkNode(null)
+                saveLink();
+                setLinkNode(null);
             }}
             onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                    saveLink()
-                    setLinkNode(null)
+                if (e.key === 'Enter') {
+                    saveLink();
+                    setLinkNode(null);
                     e.preventDefault();
                 }
             }}
