6cdab9d9e4b0b168e1889399e2fb152bb0427711
panditraghav <panditraghav18@gmail.com>
2023-04-09 17:59:05 +0530

    Added prop to add keyboard shortcut in prettier Plugin

--- packages/plugins/src/PrettierPlugin/index.tsx
+++ packages/plugins/src/PrettierPlugin/index.tsx
@@ -1,14 +1,14 @@
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useEffect } from "react";
-import { format } from "prettier/standalone";
-import { $getSelection, $isRangeSelection } from "lexical";
-import { $isCodeNode } from "@lexical/code";
-import babelParser from "prettier/parser-babel";
-import htmlParser from "prettier/parser-html";
-import markdownParser from "prettier/parser-markdown";
-import cssParser from "prettier/parser-postcss";
-import { Plugin, Options } from "prettier";
-import { LexicalEditor } from "lexical";
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { useEffect } from 'react';
+import { format } from 'prettier/standalone';
+import { $getSelection, $isRangeSelection } from 'lexical';
+import { $isCodeNode } from '@lexical/code';
+import babelParser from 'prettier/parser-babel';
+import htmlParser from 'prettier/parser-html';
+import markdownParser from 'prettier/parser-markdown';
+import cssParser from 'prettier/parser-postcss';
+import { Plugin, Options } from 'prettier';
+import { LexicalEditor } from 'lexical';
 
 interface IParser {
     lang: string[];
@@ -18,23 +18,23 @@ interface IParser {
 
 const PARSERS: IParser[] = [
     {
-        lang: ["javascript", "typescript", "js", "ts"],
-        parser: "babel",
+        lang: ['javascript', 'typescript', 'js', 'ts'],
+        parser: 'babel',
         plugins: [babelParser],
     },
     {
-        lang: ["html"],
-        parser: "html",
+        lang: ['html'],
+        parser: 'html',
         plugins: [htmlParser],
     },
     {
-        lang: ["css", "scss"],
-        parser: "css",
+        lang: ['css', 'scss'],
+        parser: 'css',
         plugins: [cssParser],
     },
     {
-        lang: ["markdown"],
-        parser: "markdown",
+        lang: ['markdown'],
+        parser: 'markdown',
         plugins: [markdownParser],
     },
 ];
@@ -54,11 +54,14 @@ function getOptions(language: string | null | undefined): Options | undefined {
     };
 }
 
-function usePrettierPlugin(editor: LexicalEditor) {
+function usePrettierPlugin(
+    editor: LexicalEditor,
+    isKeydown: (ev: KeyboardEvent) => boolean,
+) {
     useEffect(() => {
         function prettierCommandListener(ev: KeyboardEvent) {
-            if (ev.altKey) ev.preventDefault();
-            if (ev.key == "F" && ev.altKey && ev.shiftKey) {
+            if (isKeydown(ev)) {
+                ev.preventDefault();
                 editor.update(() => {
                     const selection = $getSelection();
                     if (!$isRangeSelection(selection)) return;
@@ -83,16 +86,27 @@ function usePrettierPlugin(editor: LexicalEditor) {
                 });
             }
         }
-        document.addEventListener("keydown", prettierCommandListener);
+        document.addEventListener('keydown', prettierCommandListener);
 
         return () =>
-            document.removeEventListener("keydown", prettierCommandListener);
+            document.removeEventListener('keydown', prettierCommandListener);
     }, [editor]);
 
     return null;
 }
 
-export default function PrettierPlugin() {
+function defaultIsKeydown(ev: KeyboardEvent) {
+    if (ev.altKey && ev.shiftKey && ev.key == 'F') {
+        return true;
+    }
+    return false;
+}
+
+export default function PrettierPlugin({
+    isKeydown = defaultIsKeydown,
+}: {
+    isKeydown?: (ev: KeyboardEvent) => boolean;
+}) {
     const [editor] = useLexicalComposerContext();
-    return usePrettierPlugin(editor);
+    return usePrettierPlugin(editor, isKeydown);
 }
