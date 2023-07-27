8911286367bffe0cd699b4dc27d2c04d81692953
panditraghav <panditraghav18@gmail.com>
2023-07-27 16:06:53 +0530

    Removed old colors from tailwindcss theme

--- packages/playground/tailwind.config.cjs
+++ packages/playground/tailwind.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-import colors from 'tailwindcss/colors';
 import animate from 'tailwindcss-animate';
 import scrollbar from 'tailwind-scrollbar';
 
@@ -14,58 +13,6 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'bg-dark': colors.neutral['900'],
-                'bg-light': colors.neutral['100'],
-
-                'text-dark-01': colors.neutral['900'],
-                'text-dark-02': colors.neutral['800'],
-                'text-dark-03': colors.neutral['600'],
-
-                'text-light-01': colors.neutral['50'],
-                'text-light-02': colors.neutral['100'],
-                'text-light-03': colors.neutral['300'],
-
-                'editor-bg-dark': colors.neutral['900'],
-                'editor-bg-light': colors.neutral['100'],
-
-                'editor-title-light': colors.neutral['50'],
-                'editor-title-dark': colors.neutral['900'],
-
-                'editor-h2-light': colors.neutral['100'],
-                'editor-h2-dark': colors.neutral['800'],
-
-                'editor-h3-light': colors.neutral['200'],
-                'editor-h3-dark': colors.neutral['700'],
-
-                'editor-p-light': colors.neutral['200'],
-                'editor-p-dark': colors.neutral['700'],
-
-                'editor-quote-light': colors.neutral['300'],
-                'editor-quote-dark': colors.neutral['600'],
-
-                'editor-quote-border-light': colors.neutral['300'],
-                'editor-quote-border-dark': colors.neutral['500'],
-
-                'editor-icon-light': colors.neutral['300'],
-                'editor-icon-light-hover': colors.neutral['200'],
-                'editor-icon-light-selected': colors.neutral['200'],
-
-                'editor-icon-dark': colors.neutral['700'],
-                'editor-icon-dark-hover': colors.neutral['800'],
-                'editor-icon-dark-selected': colors.neutral['800'],
-
-                'editor-item-selected-dark': colors.stone['800'],
-                'editor-item-selected-light': colors.neutral['200'],
-
-                'editor-dialog-bg-dark': colors.neutral['900'],
-                'editor-dialog-bg-light': '#f5f5f5',
-
-                'editor-button-primary': colors.blue['400'],
-                'editor-button-secondary': colors.neutral['600'],
-
-                'editor-drag-target-line-light': colors.cyan['100'],
-                'editor-drag-target-line-dark': colors.cyan['700'],
-
                 border: 'hsl(var(--border))',
                 input: 'hsl(var(--input))',
                 ring: 'hsl(var(--ring))',
