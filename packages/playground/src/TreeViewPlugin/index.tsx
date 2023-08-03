df1a525707bf3fe564434154426663bb87314ac7
panditraghav <panditraghav18@gmail.com>
2023-08-03 09:25:40 +0530

    Fixed TimeTravle theme

--- packages/playground/src/TreeViewPlugin/index.tsx
+++ packages/playground/src/TreeViewPlugin/index.tsx
@@ -8,11 +8,11 @@ export default function TreeViewPlugin() {
         <TreeView
             editor={editor}
             viewClassName="relative mt-20 mb-8 max-h-[400px] text-foreground bg-muted rounded-sm overflow-x-auto scrollbar scrollbar-thin dark:scrollbar-track-neutral-700 dark:scrollbar-thumb-neutral-600 scrollbar-track-gray-200 scrollbar-thumb-gray-400"
-            timeTravelButtonClassName="bg-red-800 aboslute top-0 right-0"
-            timeTravelPanelButtonClassName="bg-orange-800"
-            timeTravelPanelClassName="overflow-hidden mx-8 my-2 bg-orange-800 flex"
-            timeTravelPanelSliderClassName="p-0 flex-8"
-            treeTypeButtonClassName="float-right"
+            timeTravelButtonClassName="text-foreground px-2 py-1 underline"
+            timeTravelPanelButtonClassName="text-foreground px-2"
+            timeTravelPanelClassName="overflow-hidden mx-8 my-2 flex"
+            timeTravelPanelSliderClassName="w-full"
+            treeTypeButtonClassName="float-right px-2 py-1 mx-2 underline"
         />
     );
 }
